d1e24cc78ea07daf0d1a76560e91b8f624b93101
Diego Acosta <diegonake@gmail.com>
2016-05-01 01:57:37 +0200

    write to stream on parse

--- bin/index.js
+++ bin/index.js
@@ -2,74 +2,62 @@
 'use strict';
 
 const fs = require('fs');
-const request = require('request');
+const fetch = require('node-fetch');
 const cheerio = require('cheerio');
+const stream = require('JSONStream').stringify();
 
 
-const parseContent = (elem) => {
+const parseContent = (i, elem) => {
 
     if (elem.length !== 3) return null;
 
-    const op = { };
+    const inst = { opcode: `0x${(i).toString(16)}` };
 
     const l1 = elem.get(0).data.split(' ');
     const l2 = elem.get(1).data.match(/^(\w+)\s*([0-9\/]+)$/) || [];
 
-    op.mnemonic = l1[0];
+    inst.mnemonic = l1[0];
     if (l1.length > 1) {
-        op.operands = l1[1].split(',');
+        inst.operands = l1[1].split(',');
     }
     if (l2.length > 1) {
-        op.bytes = parseInt(l2[1], 10);
+        inst.bytes = parseInt(l2[1], 10);
 
         // Note:
         // For opcodes with conditional duration (e.g. CALL C,a16 3 12/24) will
         // use the first value.
 
-        op.cycles = parseInt(l2[2], 10);
+        inst.cycles = parseInt(l2[2], 10);
     }
-    op.flagsZNHC = elem.get(2).data.split(' ');
+    inst.flagsZNHC = elem.get(2).data.split(' ');
 
-    return op;
+    return inst;
 };
 
-const parseTable = ($, table) => {
-
-    const opcodes = { };
+const parseTable = ($, table, index) => {
 
     $('tr:nth-of-type(n+2)', table)
         .map((i, elem) => $('td:nth-of-type(n+2)', elem).get())
         .map((i, elem) => $(elem).contents())
         .map((i, elem) => $(elem).filter((i, elem) => elem.type == 'text'))
-        .map((i, elem) => opcodes[`0x${i.toString(16)}`] = parseContent(elem));
-
-    return opcodes;
+        .map((i, elem) => parseContent(index ? i + 0xcb00 : i, elem))
+        .each((i, elem) => stream.write(elem));
 };
 
 // References:
 // - http://www.pastraiser.com/cpu/gameboy/gameboy_opcodes.html
 // - https://github.com/NewbiZ/gbemu/tree/master/scripts
 
-const URL = 'http://www.pastraiser.com/cpu/gameboy/gameboy_opcodes.html';
-
-request(URL, function (err, response, body) {
-
-    if (err) {
+fetch('http://www.pastraiser.com/cpu/gameboy/gameboy_opcodes.html')
+    .then((res) => res.text())
+    .then((body) => {
+        const $ = cheerio.load(body);
+        $('table').splice(0, 2).forEach((v, i) => parseTable($, v, i));
+    })
+    .then(() => stream.end())
+    .catch((err) => {
         console.error(err.message);
         process.exit(1);
-    }
+    });
 
-    const $ = cheerio.load(body);
-    const tables = $('table').splice(0, 2);
-
-    const data = [
-        'unprefixed',
-        'cbprefixed'
-    ]
-    .reduce((p, c, i) => {
-        p[c] = parseTable($, tables[i]);
-        return p;
-    }, {});
-
-    process.stdout.write(JSON.stringify(data, null, 2));
-});
+stream.pipe(process.stdout);
