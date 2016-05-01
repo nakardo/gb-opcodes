feb4abbb4d0422aedaccd615c1d14b60bd9fc4c0
Diego Acosta <diegonake@gmail.com>
2016-05-01 14:03:49 +0200

    use stream properly

--- bin/index.js
+++ bin/index.js
@@ -4,12 +4,12 @@
 const fs = require('fs');
 const request = require('request');
 const cheerio = require('cheerio');
-const stream = require('JSONStream').stringify();
+const stream = require('JSONStream').stringify(false);
 
 const URL = 'http://www.pastraiser.com/cpu/gameboy/gameboy_opcodes.html';
 
 
-const parseContent = (i, elem) => {
+const parseContent = (i, elem, tableIndex) => {
 
     if (elem.length !== 3) return null;
 
@@ -17,6 +17,7 @@ const parseContent = (i, elem) => {
     const l2 = elem.get(1).data.match(/^(\w+)\s*([0-9\/]+)$/) || [];
 
     return {
+        prefix: tableIndex > 0 ? '0xcb' : undefined,
         opcode: `0x${(i).toString(16)}`,
         mnemonic: l1[0],
         operands: l1.length > 1 ? l1[1].split(',') : [],
@@ -35,7 +36,7 @@ const parseTable = ($, table, index) => {
                 .contents()
                 .filter((i, elem) => elem.type == 'text');
 
-            return parseContent(index ? i + 0xcb00 : i, textNodes);
+            return parseContent(i, textNodes, index);
         })
         .each((i, elem) => stream.write(elem));
 };
